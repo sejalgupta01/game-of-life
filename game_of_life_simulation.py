033573f2f796c23b60592b7c2b573f16fb3d5b9c
Sejal Gupta <sejal.virgo01@gmail.com>
2023-07-04 00:37:37 +0530

    Adding complete functionality to the game of life simulation using pygame

--- game_of_life_simulation.py
+++ game_of_life_simulation.py
@@ -2,7 +2,7 @@ import pygame
 import numpy as np
 
 DEFAULT_GRID_SIZE = WIDTH, HEIGHT = 800, 800
-CELL_SIZE = 10
+DEFAULT_CELL_SIZE = 10
 DEFAULT_GRID_WIDTH = 60
 DEFAULT_GRID_HEIGHT = 50
 
@@ -11,6 +11,7 @@ screen = pygame.display.set_mode(DEFAULT_GRID_SIZE)
 
 grid_width = DEFAULT_GRID_WIDTH
 grid_height = DEFAULT_GRID_HEIGHT
+cell_size = DEFAULT_CELL_SIZE
 grid = np.zeros((grid_width, grid_height), dtype=bool)
 generation = 0
 population = 0
@@ -43,12 +44,16 @@ def count_live_neighbors(x, y):
     return count
 
 def draw_grid():
+    global left_margin, top_margin, grid_rect
     screen.fill((0, 0, 0))
+    left_margin = (WIDTH - grid_width * cell_size) // 2
+    top_margin = (HEIGHT - grid_height * cell_size) // 2
+    grid_rect = pygame.Rect(left_margin, top_margin, grid_width * cell_size, grid_height * cell_size)
     pygame.draw.rect(screen, (255, 0, 0), grid_rect)
     for x in range(grid_width):
         for y in range(grid_height):
             if grid[x, y]:
-                rect = pygame.Rect(left_margin + x * CELL_SIZE, top_margin + y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                rect = pygame.Rect(left_margin + x * cell_size, top_margin + y * cell_size, cell_size, cell_size)
                 pygame.draw.rect(screen, (255, 255, 255), rect)
 
 def handle_next_button_click():
@@ -77,9 +82,9 @@ def handle_cell_click(x, y):
         population -= 1
 
 # Grid properties
-left_margin = (WIDTH - grid_width * CELL_SIZE) // 2
-top_margin = (HEIGHT - grid_height * CELL_SIZE) // 4
-grid_rect = pygame.Rect(left_margin, top_margin, grid_width * CELL_SIZE, grid_height * CELL_SIZE)
+left_margin = (WIDTH - grid_width * cell_size) // 2
+top_margin = (HEIGHT - grid_height * cell_size) // 2
+grid_rect = pygame.Rect(left_margin, top_margin, grid_width * cell_size, grid_height * cell_size)
 
 # Button properties
 button_width = 150
@@ -88,10 +93,10 @@ button_font = pygame.font.SysFont(None, 35)
 button_color = (0, 255, 0)
 
 # Top row button properties
-top_row_button_top = HEIGHT - 175
+top_row_button_top = HEIGHT - 133
 
 # Bottom row button properties
-bottom_row_button_top = HEIGHT - 100
+bottom_row_button_top = HEIGHT - 68
 
 # Populate button
 populate_button_left = (WIDTH - (100 + button_width * 3)) // 2
@@ -108,6 +113,21 @@ next_button_left = clear_button_left + button_width + 50
 next_button_rect = pygame.Rect(next_button_left, top_row_button_top, button_width, button_height)
 next_button_text = button_font.render("Next", True, (0, 0, 0))
 
+# Grid Size button
+size_button_left = (WIDTH - (100 + button_width * 3)) // 2
+size_button_rect = pygame.Rect(size_button_left, bottom_row_button_top, button_width, button_height)
+size_button_text = button_font.render("Grid Size", True, (0, 0, 0))
+
+# Evolve button
+evolve_button_left = size_button_left + button_width + 50
+evolve_button_rect = pygame.Rect(evolve_button_left, bottom_row_button_top, button_width, button_height)
+evolve_button_text = button_font.render("Evolve", True, (0, 0, 0))
+
+# Stop button
+stop_button_left = evolve_button_left + button_width + 50
+stop_button_rect = pygame.Rect(stop_button_left, bottom_row_button_top, button_width, button_height)
+stop_button_text = button_font.render("Stop", True, (0, 0, 0))
+
 running = True
 paused = True
 clock = pygame.time.Clock()
@@ -118,20 +138,116 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # elif event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_SPACE:
-        #         paused = not paused
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left mouse button
                 if next_button_rect.collidepoint(event.pos):
+                    paused = True
                     handle_next_button_click()
                 elif populate_button_rect.collidepoint(event.pos):
+                    paused = True
                     handle_populate_button_click()
                 elif clear_button_rect.collidepoint(event.pos):
+                    paused = True
                     handle_clear_button_click()
+                elif size_button_rect.collidepoint(event.pos):
+                    paused = True
+                    # Get user input for grid size
+                    width_input_box = pygame.Rect((WIDTH - 100) // 2, 300, 100, 50)
+                    height_input_box = pygame.Rect((WIDTH - 100) // 2, 400, 100, 50)             
+                    # Back button
+                    back_button_left = (WIDTH - (50 + button_width * 2)) // 2
+                    back_button_rect = pygame.Rect(back_button_left, 500, button_width, button_height)
+                    back_button_text = button_font.render("Back", True, (0, 0, 0))
+                    # Enter button
+                    enter_button_left = back_button_left + button_width + 50
+                    enter_button_rect = pygame.Rect(enter_button_left, 500, button_width, button_height)
+                    enter_button_text = button_font.render("Enter", True, (0, 0, 0))
+                    color_inactive = pygame.Color('lightskyblue3')
+                    color_active = pygame.Color('dodgerblue2')
+                    width_color = color_inactive
+                    height_color = color_inactive
+                    width_user_input = ''
+                    height_user_input = ''
+                    active = True
+
+                    while active:
+                        for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                pygame.quit()
+                                running = False
+                                active = False
+                            if event.type == pygame.MOUSEBUTTONDOWN:
+                                if width_input_box.collidepoint(event.pos):
+                                    active = True
+                                    width_color = color_active
+                                    height_color = color_inactive
+                                elif height_input_box.collidepoint(event.pos):
+                                    active = True
+                                    width_color = color_inactive
+                                    height_color = color_active
+                                elif back_button_rect.collidepoint(event.pos):
+                                    active = False
+                                    width_color = color_inactive
+                                    height_color = color_inactive
+                                elif enter_button_rect.collidepoint(event.pos):
+                                    if active:
+                                        try:
+                                            new_width, new_height = int(width_user_input.strip()), int(height_user_input.strip())
+                                            new_cell_size = min((DEFAULT_GRID_WIDTH * DEFAULT_CELL_SIZE) // new_width, (DEFAULT_GRID_HEIGHT * DEFAULT_CELL_SIZE) // new_height)
+                                            if new_width > 0 and new_height > 0 and new_cell_size > 0:
+                                                grid_width, grid_height = new_width, new_height
+                                                cell_size = new_cell_size
+                                                grid = np.zeros((grid_width, grid_height), dtype=bool)
+                                                active = False
+                                                color = color_inactive
+                                                width_user_input = ''
+                                                height_user_input = ''
+                                                generation = 0
+                                                draw_grid()
+                                        except ValueError:
+                                            width_user_input = ''
+                                            height_user_input = ''
+                                else:
+                                    width_color = color_inactive
+                                    height_color = color_inactive
+                            elif event.type == pygame.KEYDOWN:
+                                if active:
+                                    if event.key == pygame.K_BACKSPACE:
+                                        if width_color == color_active:
+                                            width_user_input = width_user_input[:-1]
+                                        elif height_color == color_active:
+                                            height_user_input = height_user_input[:-1]
+                                    else:
+                                        if width_color == color_active:
+                                            width_user_input += event.unicode
+                                        elif height_color == color_active:
+                                            height_user_input += event.unicode
+                        screen.fill((0, 0, 0))
+                        font = pygame.font.SysFont(None, 24)
+                        txt_width = font.render("Width: ", True, width_color)
+                        screen.blit(txt_width, ((WIDTH - 100) // 2, width_input_box.y - 20))
+                        txt_height = font.render("Height: ", True, height_color)
+                        screen.blit(txt_height, ((WIDTH - 100) // 2, height_input_box.y - 20))
+                        input_font = pygame.font.SysFont(None, 35)
+                        input_width = input_font.render(width_user_input, True, width_color)
+                        screen.blit(input_width, ((WIDTH - 100) // 2 + (100 - input_width.get_width()) // 2, 300 + (50 - input_width.get_height()) // 2))
+                        input_height = input_font.render(height_user_input, True, height_color)
+                        screen.blit(input_height, ((WIDTH - 100) // 2 + (100 - input_height.get_width()) // 2, 400 + (50 - input_height.get_height()) // 2))
+                        pygame.draw.rect(screen, width_color, width_input_box, 2)
+                        pygame.draw.rect(screen, height_color, height_input_box, 2)
+                        pygame.draw.rect(screen, button_color, back_button_rect)
+                        screen.blit(back_button_text, (back_button_left + (button_width - back_button_text.get_width()) // 2, 500 + (button_height - back_button_text.get_height()) // 2))
+                        pygame.draw.rect(screen, button_color, enter_button_rect)
+                        screen.blit(enter_button_text, (enter_button_left + (button_width - enter_button_text.get_width()) // 2, 500 + (button_height - enter_button_text.get_height()) // 2))
+                        pygame.display.flip()
+                elif evolve_button_rect.collidepoint(event.pos):
+                    paused = False
+                elif stop_button_rect.collidepoint(event.pos):
+                    paused = True 
                 elif grid_rect.collidepoint(event.pos):
-                    cell_x = (event.pos[0] - left_margin) // CELL_SIZE
-                    cell_y = (event.pos[1]  - top_margin) // CELL_SIZE
+                    paused = True
+                    cell_x = (event.pos[0] - left_margin) // cell_size
+                    cell_y = (event.pos[1]  - top_margin) // cell_size
                     handle_cell_click(cell_x, cell_y)
         if not paused:
             grid = update_grid()
@@ -140,11 +256,11 @@ while running:
 
     # Generation text
     gen_text = button_font.render("Generation: " + str(generation), True, (255, 255, 255))
-    screen.blit(gen_text, (left_margin, top_margin - gen_text.get_height() - 10))
+    screen.blit(gen_text, (100, 100))
 
     # Population text
     pop_text = button_font.render("Population: " + str(population), True, (255, 255, 255))
-    screen.blit(pop_text, (left_margin + grid_width * CELL_SIZE - pop_text.get_width(), top_margin - gen_text.get_height() - 10))
+    screen.blit(pop_text, (WIDTH - 100 - pop_text.get_width(), 100))
 
     pygame.draw.rect(screen, button_color, populate_button_rect)
     screen.blit(populate_button_text, (populate_button_left + (button_width - populate_button_text.get_width()) // 2, top_row_button_top + (button_height - populate_button_text.get_height()) // 2))
@@ -152,6 +268,12 @@ while running:
     screen.blit(clear_button_text, (clear_button_left + (button_width - clear_button_text.get_width()) // 2, top_row_button_top + (button_height - clear_button_text.get_height()) // 2))
     pygame.draw.rect(screen, button_color, next_button_rect)
     screen.blit(next_button_text, (next_button_left + (button_width - next_button_text.get_width()) // 2, top_row_button_top + (button_height - next_button_text.get_height()) // 2))
+    pygame.draw.rect(screen, button_color, size_button_rect)
+    screen.blit(size_button_text, (size_button_left + (button_width - size_button_text.get_width()) // 2, bottom_row_button_top + (button_height - size_button_text.get_height()) // 2))
+    pygame.draw.rect(screen, button_color, evolve_button_rect)
+    screen.blit(evolve_button_text, (evolve_button_left + (button_width - evolve_button_text.get_width()) // 2, bottom_row_button_top + (button_height - evolve_button_text.get_height()) // 2))
+    pygame.draw.rect(screen, button_color, stop_button_rect)
+    screen.blit(stop_button_text, (stop_button_left + (button_width - stop_button_text.get_width()) // 2, bottom_row_button_top + (button_height - stop_button_text.get_height()) // 2))
 
     pygame.display.flip()
 
